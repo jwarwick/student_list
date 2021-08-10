6dce3b01c26d65cac7076cc350bc75005582f462
John Warwick <jwarwick@gmail.com>
2021-08-10 17:43:49 -0400

    Updating prod hostname

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :student_list, StudentListWeb.Endpoint,
-  # url: [host: "example.com", port: 80],
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  url: [host: "#{System.get_env("APP_NAME")}.gigalixirapp.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
