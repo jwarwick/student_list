f81bfaa577845891300da64603355e93286272ab
John Warwick <jwarwick@gmail.com>
2021-08-11 15:00:18 -0400

    Handling socket connection from different hostname

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,8 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :student_list, StudentListWeb.Endpoint,
-  url: [host: "#{System.get_env("APP_NAME")}.gigalixirapp.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
 config :logger, level: :info
