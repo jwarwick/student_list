83251a29628d3c56ee8924e394931c22a1ba56f5
John Warwick <jwarwick@gmail.com>
2021-08-13 12:08:59 -0400

    Added hero logo

--- lib/student_list_web/live/heading.ex
+++ lib/student_list_web/live/heading.ex
@@ -10,15 +10,22 @@ defmodule StudentListWeb.Live.Heading do
     ~F"""
     <section class="hero is-primary">
       <div class="hero-body">
-        <p class="title">
-          Add your information!
-        </p>
-        <p class="subtitle">
-          Enter your information to be included in the student directory. All fields are optional.
-        </p>
-        <p :if={@support_email} class="subtitle">
-          Questions or comments? Email <a href={"mailto:#{@support_email}?Subject=Support"}>{@support_email}</a>
-        </p>
+        <div class="columns is-vcentered">
+          <figure class="column is-narrow image">
+            <img class="hero-logo" src="/images/logo.png")>
+          </figure>
+          <div class="column">
+            <p class="title">
+              Add your information!
+            </p>
+            <p class="subtitle">
+              Enter your information to be included in the student directory. All fields are optional.
+            </p>
+            <p :if={@support_email} class="subtitle">
+              Questions or comments? Email <a href={"mailto:#{@support_email}?Subject=Support"}>{@support_email}</a>
+            </p>
+          </div>
+        </div>
       </div>
     </section>
     """
